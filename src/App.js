ce610f9ad951e0c8c4a5cf7499c92ab94e596fe1
Max Rodrigues <maxuel.rodrigues@gmail.com>
2021-11-04 18:55:37 -0300

    aula 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWord'
 
 function App() {
+
+  const name = 'Max Rodrigues';
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá React</h1>
+      <p>Meu primeiro app</p>
+      <p>Olá {newName}</p>
+      <p>Soma: {sum(2, 2)}</p>
+
+      <img src={url} alt="Minha imagem"/>
+
+      <HelloWorld />
     </div>
   );
 }
